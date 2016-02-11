40705f50988a62c3793fa74a79aa4eb7c1807bf5
Justin McGuire <jm@landedstar.com>
2016-02-11 16:04:06 -0500

    command line app basically works
     - Add logging. Change level to DEBUG to get debug info.
     - Refactored usage info
     - fixed tag errors and punctuation mistakes in config file

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ import random
 import re
 import yaml
 import os
+import logging
 
 class ToolConfig:
 
@@ -29,17 +30,18 @@ class ToolConfig:
     self.generate  = self.config['meta']['generate']
     self.start     = self.config['meta']['start']
     self.norepeats = True
+    self.saved_tags = {}
 
   def load_config(self, config_file):
     """load the config file into the static config variable, but only once"""
-    if os.path.isfile(config_file):
-      pass
+    if not os.path.isfile(config_file):
+      sys.exit("config file: %s is not a file" % config_file)
 
     if not self.config:
       with open(config_file) as file:
         self.config = yaml.load(file)
 
-  def start(self):
+  def create(self):
     """get an random selection."""
 
     # if we don't care about repeats, reload the config file after every use
@@ -47,9 +49,13 @@ class ToolConfig:
       self.backup_config = self.config
 
     # start the string with the "start" variable
-    select = self.get_random_item_from( self.config[self.start] )
-    self.interpolate(select)
-    self.saved_names = {}
+    select = self.get_random_item_from( self.start )
+    logging.debug("inital string %s" % select)
+    select = self.interpolate(select)
+
+    # these get set in interpolate, but must be unset elsewhere, since it's a
+    # recursive function that doesn't know when its time is over
+    self.saved_tags = {}
 
     if not self.norepeats:
       self.config = self.backup_config
@@ -58,8 +64,9 @@ class ToolConfig:
 
   def get_random_item_from(self, listname):
     """remove a random item from one of the lists in the config, and return it"""
-    pick = random.randint(0, config[listname].length - 1)
-    return config[listname].pop(pick)
+
+    pick = random.randint(0, len(self.config[listname]) - 1)
+    return self.config[listname].pop(pick)
 
   def interpolate(self, string):
     """replace references in string with other items from hash, recursive"""
@@ -67,44 +74,62 @@ class ToolConfig:
     # look for a reference, which looks like [hashname]
     m = re.search(r'\[([^]]*)\]', string)
     if m:
-      tag = m.group(0)
+      tag = m.group(1)
+
+      logging.debug("found tag %s" % tag)
 
       # the listname may need to be saved, so it can be reused later
-      (list_name, saved_name) = tag.split(':')
+      if ':' in tag:
+        (list_name, saved_tag) = tag.split(':')
+      else:
+        list_name = tag
+        saved_tag = ''
+
+      logging.debug("tag split into list_name/saved_tag: %s/%s" % (list_name, saved_tag))
 
       # get the new selection to replace the tag with
       selection = ''
-      if self.config[list_name]:
-        # check the saved variables first
-        selection = self.config[saved_name]
+      if list_name in self.saved_tags:
+        # check if the list_name is actually a saved tag
+        selection = self.saved_tags[list_name]
       else:
         # otherwise grab a random selection from the choosen list
         selection = self.get_random_item_from(list_name)
 
       # if we want to save the selection, do that now
-      if saved_name:
-        self.config[saved_name] = selection
+      if saved_tag:
+        self.saved_tags[saved_tag] = selection
 
       # there may be more interpolation
-      string = self.interpolate( string.replace('[%s]' % tag, selection) )
+      logging.debug("replacing [%s] with %s" % (tag, selection))
+      string = self.interpolate( string.replace('[%s]' % tag, selection, 1) )
 
     return string
 
 def main(config_file):
-  config = ToolConfig(config_file)
-  print config.title
+  logging.basicConfig(level=logging.WARNING)
+  tool_config = ToolConfig(config_file)
+  print tool_config.title
 
   # print out each random selection
-  for x in range(config.generate):
-    item = config.start()
-    print "%d: %s\n" % (x, item)
+  for x in range(tool_config.generate):
+    item = tool_config.create()
+    print "%d: %s\n" % (x+1, item)
+
+def usage(error_msg=''):
+  usage_msg = "usage: %s <config_file>" % sys.argv[0]
+  if error_msg:
+    sys.exit("%s\n%s" % (error_msg, usage_msg))
+  else:
+    sys.exit(usage_msg)
 
 if __name__ == '__main__':
+  # make sure our arguments are correct
   if len(sys.argv) > 1:
     config_file = sys.argv[1]
     if not os.path.isfile(config_file):
-      sys.exit("config file %s isn't a file\nusage: %s <config_file>" % (config_file, sys.argv[0]))
+      usage("config file %s isn't a file" % config_file)
     main(config_file)
   else:
-    sys.exit("usage: %s <config_file>" % sys.argv[0])
+    usage()
 
